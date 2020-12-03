6d0ecc3d63264d21158879c9126da9dff8299c2f
henryaugusta <henryaugusta4@gmail.com>
2020-12-04 01:29:36 +0700

    Weekend ga boleh ngoding

---
+++ MODUL5 HENRY/app/Http/Controllers/ProductController.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class ProductController extends Controller
+{
+    /**
+     * index
+     *
+     * @return void
+     */
+    public function index()
+    {
+        $product = Product::latest()->paginate(10);
+        return view('product.index', compact('product'));
+    }
+
+    /**
+     * create
+     *
+     * @return void
+     */
+    public function create()
+    {
+        return view('product.create');
+    }
+
+
+    // protected $fillable = [
+    //     'name', 'price', 'description','stock','img_path'
+    // ];
+
+
+
+
+
+    /**
+     * store
+     *
+     * @param  mixed $request
+     * @return void
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'image'     => 'required|image|mimes:png,jpg,jpeg',
+            'title'     => 'required',
+            'price'     => 'required',
+            'stock'     => 'required',
+            'description'   => 'required'
+        ]);
+
+        //upload image
+        $image = $request->file('image');
+        $image->storeAs('public/products', $image->hashName());
+
+        $product = Product::create([
+            'image'     => $image->hashName(),
+            'name'     => $request->title,
+            'description'     => $request->description,
+            'stock'     => $request->stock,
+            'img_path'     => $image->hashName(),
+            'price'     => $request->price,
+            'content'   => $request->content
+        ]);
+
+        if ($product) {
+            //redirect dengan pesan sukses
+            return redirect()->route('product.index')->with(['success' => 'Data Berhasil Disimpan!']);
+        } else {
+            //redirect dengan pesan error
+            return redirect()->route('producr.index')->with(['error' => 'Data Gagal Disimpan!']);
+        }
+    }
+
+
+    /**
+     * edit
+     *
+     * @param  mixed $product
+     * @return void
+     */
+    public function edit(Product $product)
+    {
+        return view('product.edit', compact('product'));
+    }
+
+
+    public function destroy($id)
+    {
+        $product = Product::findOrFail($id);
+        //hapus old image
+        Storage::disk('local')->delete('public/storage/product/' . $product->img_path);
+        $product->delete();
+
+        if ($product) {
+            //redirect dengan pesan sukses
+            return redirect()->route('product.index')->with(['success' => 'Data Berhasil Dihapus!']);
+        } else {
+            //redirect dengan pesan error
+            return redirect()->route('product.index')->with(['error' => 'Data Gagal Dihapus!']);
+        }
+    }
+
+
+    /**
+     * update
+     *
+     * @param  mixed $request
+     * @param  mixed $blog
+     * @return void
+     */
+    public function update(Request $request, Product $product)
+    {
+        $this->validate($request, [
+            // 'image'     => 'required|image|mimes:png,jpg,jpeg',
+            'title'     => 'required',
+            'price'     => 'required',
+            'stock'     => 'required',
+            'description'   => 'required'
+        ]);
+
+
+        //get data Blog by ID
+        $product = Product::findOrFail($product->id);
+
+        if ($request->file('image') == "") {
+
+            $product->update([
+                'name'     => $request->title,
+                'description'     => $request->description,
+                'stock'     => $request->stock,
+                'price'     => $request->price,
+                'content'   => $request->content
+            ]);
+        } else {
+
+            //hapus old image
+            Storage::disk('local')->delete('public/storage/product/' . $product->img_path);
+
+            //upload new image
+            //upload image
+            $image = $request->file('image');
+            $image->storeAs('public/products', $image->hashName());
+            $product->update([
+                'image'     => $image->hashName(),
+                'name'     => $request->title,
+                'description'     => $request->description,
+                'stock'     => $request->stock,
+                'img_path'     => $image->hashName(),
+                'price'     => $request->price,
+                'content'   => $request->content
+            ]);
+        }
+
+        if ($product) {
+            //redirect dengan pesan sukses
+            return redirect()->route('product.index')->with(['success' => 'Data Berhasil Diupdate!']);
+        } else {
+            //redirect dengan pesan error
+            return redirect()->route('product.index')->with(['error' => 'Data Gagal Diupdate!']);
+        }
+    }
+}
---
+++ MODUL5 HENRY/database/migrations/2020_12_03_154527_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('products', function (Blueprint $table) {
+          $table->id();
+          $table->string('name');
+          $table->double('price');
+          $table->text('description');
+          $table->integer('stock');
+          $table->string('img_path');
+          $table->timestamp('created_at')->nullable();
+          $table->timestamp('updated_at')->nullable();
+          
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
